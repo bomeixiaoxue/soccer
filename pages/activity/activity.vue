960000a67411fc30b1ddd368a2e691f7efa5926a
Lin sir <2267919161@qq.com>
2019-04-02 00:17:43 +0800

    创建足球社区项目

---
+++ pages/activity/activity.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="uni-bg-blue">
+		动态页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
